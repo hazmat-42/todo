c44ed06ff1ad7cf1125ddcec305858b5cfbe975b
Timothy Chatman <hazmat42@protonmail.com>
2026-05-15 22:53:32 -0400

    Create functions for init_list and create item in todo.c.

--- src/todo.c
+++ src/todo.c
@@ -17,3 +17,20 @@ typedef struct list {
     char    *name;
     Item    *items;
 } List;
+
+// initialize empty list
+List* init_list(void) {
+    List* list = (List*)malloc(sizeof(List));
+    return list;
+}
+
+// create an individual item
+Item* create_item(char* name, u16 position, u8 priority, char* description, bool completed) {
+    Item* new_item = (Item*)malloc(sizeof(Item));
+    new_item->item = name;
+    new_item->position = position;
+    new_item->priority = priority;
+    new_item->description = description;
+    new_item->completed = completed;
+    return new_item;
+}
