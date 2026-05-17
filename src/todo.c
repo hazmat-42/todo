824980909785210e74370d3b1a5674f52a5bd7c2
Timothy Chatman <hazmat42@protonmail.com>
2026-05-17 14:07:09 -0400

    Minor edits.

--- src/todo.c
+++ src/todo.c
@@ -3,22 +3,6 @@
 //
 #include "todo.h"
 
-// individual items on the list
-typedef struct item {
-    char    *item;
-    u16     position;
-    u8      priority;
-    char    *description;
-    bool    completed;
-} Item;
-
-// Struct for actual list
-typedef struct list {
-    char    *name;
-    Item    *items;
-    i16      size;
-    i16      capacity;
-} List;
 
 // initialize empty list
 List* init_list(void) {
--- src/todo.h
+++ src/todo.h
@@ -19,4 +19,19 @@ typedef uint32_t u32;
 typedef int64_t i64;
 typedef uint64_t u64;
 
+// individual items on the list
+typedef struct item {
+    char    *item;
+    u16     position;
+    u8      priority;
+    char    *description;
+    bool    completed;
+} Item;
 
+// Struct for actual list
+typedef struct list {
+    char    *name;
+    Item    *items;
+    i16      size;
+    i16      capacity;
+} List;
